5240c8fb7b94ee777fadbc47f9240f34d3d676b3
zigsong <wldms5764@gmail.com>
2024-07-13 11:41:57 +0900

    feat: create notescreen textfield

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -11,13 +11,62 @@ class _NoteScreenState extends State<NoteScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade100,
-      body: Center(
-        child: ElevatedButton(
+      appBar: AppBar(
+          backgroundColor: Colors.grey.shade50,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
             onPressed: () {
               Navigator.pop(context);
             },
-            child: const Text('Go Back!')),
+          )),
+      backgroundColor: Colors.grey.shade50,
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  height: 300,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  margin: const EdgeInsets.only(bottom: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 4,
+                        offset: const Offset(2, 2),
+                        color: Colors.black.withOpacity(0.1),
+                      )
+                    ],
+                  ),
+                  child: const TextField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                  )),
+              Container(
+                height: 24,
+                width: 24,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 4,
+                      offset: const Offset(2, 2),
+                      color: Colors.black.withOpacity(0.1),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
