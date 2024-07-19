a88649aa285627c304b06009c3ea10492e783c14
zigsong <wldms5764@gmail.com>
2024-07-19 20:18:14 +0900

    feat: refactor note screen & add confirm dialog

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -27,25 +27,37 @@ class _NoteScreenState extends State<NoteScreen> {
             crossAxisAlignment: CrossAxisAlignment.end,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              TextButton(
+                style: TextButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 16),
+                    padding: const EdgeInsets.all(0)),
+                onPressed: () => showConfirmDialog(context),
+                child: const Text('완료'),
+              ),
               Container(
-                  height: 300,
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  margin: const EdgeInsets.only(bottom: 16),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 4,
-                        offset: const Offset(2, 2),
-                        color: Colors.black.withOpacity(0.1),
-                      )
-                    ],
-                  ),
-                  child: const TextField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                  )),
+                height: 300,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                margin: const EdgeInsets.only(bottom: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 4,
+                      offset: const Offset(2, 2),
+                      color: Colors.black.withOpacity(0.1),
+                    )
+                  ],
+                ),
+                child: const TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                      hintText: "감정일기를 작성해보세요",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(vertical: 20)),
+                ),
+              ),
               Container(
                 height: 24,
                 width: 24,
@@ -67,6 +79,26 @@ class _NoteScreenState extends State<NoteScreen> {
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Future<String?> showConfirmDialog(BuildContext context) {
+    return showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('일기 등록'),
+        content: const Text('완료 후에는 수정이 불가합니다.\n정말 완료하시겠습니까?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Ok'),
+            child: const Text('OK'),
+          ),
+        ],
       ),
     );
   }
