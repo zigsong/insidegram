db85473da29ddcb863e776a3d2852a96bbec8ba1
zigsong <wldms5764@gmail.com>
2024-07-27 17:52:55 +0900

    feat: diary detail screen

--- lib/models/diary_detail_model.dart
+++ lib/models/diary_detail_model.dart
@@ -1,21 +1,23 @@
 import 'package:insidegram/models/emotion_comment_model.dart';
 
-class DiaryModel {
+class DiaryDetailModel {
   final int diary_id;
   final String user_id, content;
   final String? memory_content, created_at;
   final Emotion? main_emotion;
+  final List<EmotionCommentModel> reaction;
 
-  DiaryModel(this.diary_id, this.user_id, this.content, this.created_at,
-      this.main_emotion, this.memory_content);
+  DiaryDetailModel(this.diary_id, this.user_id, this.content, this.created_at,
+      this.main_emotion, this.memory_content, this.reaction);
 
-  DiaryModel.fromJson(Map<String, dynamic> json)
+  DiaryDetailModel.fromJson(Map<String, dynamic> json)
       : diary_id = json['diary_id'],
         user_id = json['user_id'],
         content = json['content'],
         created_at = json['created_at'],
         main_emotion = _emotionFromString(json['main_emotion']),
-        memory_content = json['memory_content'];
+        memory_content = json['memory_content'],
+        reaction = _commentsFromReaction(json['reaction']);
 
   static Emotion _emotionFromString(String emotion) {
     switch (emotion) {
@@ -30,5 +32,13 @@ class DiaryModel {
       default:
         throw Exception('Unknown emotion type');
     }
+  }
+
+  static _commentsFromReaction(dynamic reaction) {
+    final List<EmotionCommentModel> result = [];
+    for (var comment in reaction) {
+      result.add(EmotionCommentModel.fromJson(comment));
+    }
+    return result;
   }
 }
--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,24 +1,146 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class DetailScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:insidegram/models/diary_detail_model.dart';
+import 'package:insidegram/screens/note_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:http/http.dart' as http;
+
+class DetailScreen extends StatefulWidget {
   final String content;
+  final int diary_id;
 
   const DetailScreen({
     super.key,
+    required this.diary_id,
     required this.content,
   });
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  final supabase = Supabase.instance.client;
+
+  static const String baseUrl = "http://223.130.159.43:8000";
+  late Future<DiaryDetailModel?> _diary;
+
+  @override
+  void initState() {
+    super.initState();
+    _diary = fetchDiary();
+  }
+
+  Future<DiaryDetailModel?> fetchDiary() async {
+    final accessToken = supabase.auth.currentSession?.accessToken;
+
+    if (accessToken == null) {
+      return null;
+    }
+
+    final url = Uri.parse('$baseUrl/diary/${widget.diary_id}');
+    final response = await http.get(
+      url,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        'Authorization': accessToken,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final result = jsonDecode(utf8.decode(response.bodyBytes));
+
+      print('result: $result');
+
+      return DiaryDetailModel.fromJson(result);
+    }
+
+    throw Error();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+          backgroundColor: const Color(0xff54336F),
           leading: IconButton(
-        icon: const Icon(Icons.arrow_back),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      )),
-      body: Center(child: Text(content)),
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context, 'Refetch');
+            },
+          )),
+      backgroundColor: const Color(0xff54336F),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        margin: const EdgeInsets.only(bottom: 60),
+        child: FutureBuilder(
+          future: _diary,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasData) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 300,
+                    width: 300,
+                    padding: const EdgeInsets.all(24),
+                    margin: const EdgeInsets.only(bottom: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(40),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 4,
+                          offset: const Offset(2, 2),
+                          color: Colors.black.withOpacity(0.1),
+                        )
+                      ],
+                    ),
+                    child: Text(snapshot.data!.content),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 16),
+                        EmotionComments(
+                            emotionComments: snapshot.data!.reaction),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return const Center(child: Text('No data'));
+            }
+          },
+        ),
+      ),
     );
+    // return Scaffold(
+    //     appBar: AppBar(
+    //         leading: IconButton(
+    //       icon: const Icon(Icons.arrow_back),
+    //       onPressed: () {
+    //         Navigator.pop(context);
+    //       },
+    //     )),
+    //     body: FutureBuilder(
+    //       future: _diary,
+    //       builder: (context, snapshot) {
+    //         if (snapshot.connectionState == ConnectionState.waiting) {
+    //           return const Center(child: CircularProgressIndicator());
+    //         } else if (snapshot.hasData) {
+    //           return Expanded(
+    //               child: Center(child: Text(snapshot.data!.content)));
+    //         } else {
+    //           return const Center(child: Text('No data'));
+    //         }
+    //       },
+    //     ));
   }
 }
