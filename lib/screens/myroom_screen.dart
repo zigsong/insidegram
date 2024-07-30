5a043a4102d0f15286f901ab559b525710abe339
zigsong <wldms5764@gmail.com>
2024-07-30 21:19:41 +0900

    feat: myroom layout touch, add logout button

--- lib/screens/myroom_screen.dart
+++ lib/screens/myroom_screen.dart
@@ -98,136 +98,164 @@ class _MyroomScreenState extends State<MyroomScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff54336F),
-      body: Align(
-        alignment: FractionalOffset.bottomCenter,
-        child: Column(
+      body: SafeArea(
+        child: Stack(
           children: [
-            const SizedBox(
-              height: 40,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  constraints: const BoxConstraints(),
+                  icon: const Icon(
+                    Icons.logout_rounded,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
             ),
-            SizedBox(
-              height: 300,
-              child: Center(
+            Center(
                 child: FutureBuilder(
-                  future: _main_emotion,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (snapshot.hasData) {
-                      return Transform.translate(
-                        offset: const Offset(0, 56),
-                        child: Column(
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 32, vertical: 16),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: colorMap[snapshot.data!]),
-                              child: Text(
-                                messageMap[snapshot.data!] ?? '',
-                                style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w600),
-                              ),
-                            ),
-                            const SizedBox(height: 20),
-                            Image(
-                              image: AssetImage(imageMap[snapshot.data!] ?? ''),
-                              width: 240,
-                            ),
-                          ],
+              future: _main_emotion,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasData) {
+                  return Transform.translate(
+                    offset: const Offset(0, 64),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 32, vertical: 16),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: colorMap[snapshot.data!]),
+                          child: Text(
+                            messageMap[snapshot.data!] ?? '',
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600),
+                          ),
                         ),
-                      );
-                    } else {
-                      return const Center(child: Text('No data'));
-                    }
-                  },
+                        const SizedBox(height: 20),
+                        Image(
+                          image: AssetImage(imageMap[snapshot.data!] ?? ''),
+                          width: 240,
+                        ),
+                      ],
+                    ),
+                  );
+                } else {
+                  return const Center(child: Text('No data'));
+                }
+              },
+            )),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Image(
+                      image: AssetImage('assets/images/control_panel.png'),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(color: Color(0xfff0f0f0)),
+                      child: Column(
+                        children: [
+                          Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 20),
+                                child: Image(
+                                  image: AssetImage(
+                                      'assets/images/myroom_door.png'),
+                                  // width: 320,
+                                ),
+                              ),
+                              const Positioned(
+                                left: 0,
+                                bottom: 0,
+                                child: Image(
+                                  image: AssetImage(
+                                      'assets/images/memory_grandma.png'),
+                                  width: 180,
+                                ),
+                              ),
+                              const Positioned(
+                                right: 0,
+                                bottom: 0,
+                                child: Image(
+                                  image: AssetImage(
+                                      'assets/images/memory_book.png'),
+                                  width: 220,
+                                ),
+                              ),
+                              Positioned.fill(
+                                  child: Align(
+                                alignment: Alignment.center,
+                                child: FutureBuilder(
+                                  future: _memoryText,
+                                  builder: (context, snapshot) {
+                                    if (snapshot.connectionState ==
+                                        ConnectionState.waiting) {
+                                      return const CircularProgressIndicator();
+                                    } else if (snapshot.hasData) {
+                                      return ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            _memoryText = fetchMemory();
+                                          });
+                                          showDialog(
+                                              context: context,
+                                              builder: (context) {
+                                                return Dialog(
+                                                    child: Container(
+                                                        padding:
+                                                            const EdgeInsets
+                                                                .symmetric(
+                                                                horizontal: 20,
+                                                                vertical: 28),
+                                                        child: Wrap(
+                                                          children: [
+                                                            const Text(
+                                                                '2023년 3월 3일에 작성한 일기\n'),
+                                                            const SizedBox(
+                                                              height: 16,
+                                                            ),
+                                                            Text(
+                                                                snapshot.data!),
+                                                          ],
+                                                        )));
+                                              });
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            foregroundColor: Colors.black,
+                                            backgroundColor:
+                                                const Color(0xffE3D6D7),
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 24, vertical: 12)),
+                                        child: const Text(
+                                            '홍홍홍.. 옛날에 이런일이 있었다지.\n추억을 한 번 보겠니?'),
+                                      );
+                                    } else {
+                                      return const Center();
+                                    }
+                                  },
+                                ),
+                              )),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
-            const Image(
-              image: AssetImage('assets/images/control_panel.png'),
-            ),
-            Container(
-              decoration: const BoxDecoration(color: Color(0xfff0f0f0)),
-              child: Stack(
-                clipBehavior: Clip.none,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Image(
-                      image: AssetImage('assets/images/myroom_door.png'),
-                      // width: 320,
-                    ),
-                  ),
-                  const Positioned(
-                    left: 0,
-                    bottom: 0,
-                    child: Image(
-                      image: AssetImage('assets/images/memory_grandma.png'),
-                      width: 200,
-                    ),
-                  ),
-                  const Positioned(
-                    right: 0,
-                    bottom: 0,
-                    child: Image(
-                      image: AssetImage('assets/images/memory_book.png'),
-                      width: 200,
-                    ),
-                  ),
-                  Positioned.fill(
-                      child: Align(
-                    alignment: Alignment.center,
-                    child: FutureBuilder(
-                      future: _memoryText,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const CircularProgressIndicator();
-                        } else if (snapshot.hasData) {
-                          return ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                _memoryText = fetchMemory();
-                              });
-                              showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return Dialog(
-                                        child: Container(
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 20, vertical: 28),
-                                            child: Wrap(
-                                              children: [
-                                                const Text(
-                                                    '2023년 3월 3일에 작성한 일기\n'),
-                                                const SizedBox(
-                                                  height: 16,
-                                                ),
-                                                Text(snapshot.data!),
-                                              ],
-                                            )));
-                                  });
-                            },
-                            style: ElevatedButton.styleFrom(
-                                foregroundColor: Colors.black,
-                                backgroundColor: const Color(0xffE3D6D7),
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 24, vertical: 12)),
-                            child: const Text(
-                                '홍홍홍.. 옛날에 이런일이 있었다지.\n추억을 한 번 보겠니?'),
-                          );
-                        } else {
-                          return const Center();
-                        }
-                      },
-                    ),
-                  )),
-                ],
-              ),
-            )
           ],
         ),
       ),
